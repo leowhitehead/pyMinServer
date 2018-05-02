36bf3819bde6479c62075678359dc2e8993eb4e6
x89k <lduck11007@gmail.com>
2018-05-02 10:13:21 +1000

    update

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
 import pyMinServer
 
-def main(request, **headers):
-    print "request: GET {}".format(request)
-    if request.startswith("/index"):
-        return "<html><body><h1>Index Page!</h1><img src=\"img.jpg\"></body></html>"
-    elif request.endswith((".png", ".jpg", ".jpeg")):
-        return open(request[1:], "rb").read()
-
+def main(request, **header):
+    """A simple example site demo. main() handles all get requests.
+    'request' is the request path, '**header' is the"""
+    print "Request: GET {}".format(request)
+    request = request[1:]
+    if request in ["", "index", "index.html", "index.htm"]:
+        return "<html><h1>Index of site</h1></html>"
+    elif request in ["about", "about.html", "about.htm"]:
+        return "<html><h1>About Page</h1></html>"
+    elif request == "favicon.ico":
+        return open("image.jpg", "rb").read()
+    else:
+        return "<html><h1>Error 404</h1><br><p>File Not Found</p></html>"
 
 if __name__ == "__main__":
     pyMinServer.start()
